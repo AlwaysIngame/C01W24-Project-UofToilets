64712794c10214195477de3f50886e21669a41c7
unknown <kieran.r.hansen@gmail.com>
2024-03-03 18:52:36 -0500

    Began work on popup menu with colitis information

---
+++ app/components/userCardMenu.js
@@ -0,0 +1,54 @@
+import { BottomSheet, SheetContent } from 'react-spring-bottom-sheet'
+import { Button, StyleSheet, Text, View } from 'react-native';
+
+// if setting up the CSS is tricky, you can add this to your page somewhere:
+// <link rel="stylesheet" href="https://unpkg.com/react-spring-bottom-sheet/dist/style.css" crossorigin="anonymous">
+import 'react-spring-bottom-sheet/dist/style.css'
+
+const CardForm = () => {
+  return (
+    <>
+      <BottomSheet 
+      open 
+      skipInitialTransition 
+      blocking={false}
+      defaultSnap={({ maxHeight }) => maxHeight / 4}
+      //These values can be changed according to page requirements
+      snapPoints={({ maxHeight }) => [ 
+        maxHeight / 1.5,
+        maxHeight / 4,
+      ]}
+      expandOnContentDrag
+      >
+        <View>
+          <Text style={styles.diseaseName}><b>Ulcerative Colitis</b></Text>
+          <Text style={styles.diseaseDesc}>I live with colitis, a medical condition requiring urgent use of the washroom. Thank you for your understanding and cooperation.</Text>
+          <Button style={styles.cccInfoButton}>Hello</Button>
+          <Button style={styles.gohereButton}>Hello again</Button>
+        </View>
+      </BottomSheet>
+    </>
+  )
+}
+
+const styles = StyleSheet.create({
+  diseaseName: {
+    marginTop: '2vh',
+    marginLeft: '3vw',
+    fontSize: '6vw',
+  },
+  diseaseDesc: {
+    marginTop: '3vh',
+    marginLeft: '3vw',
+    fontSize: '3.75vw',
+  },
+  cccInfoButton: {
+    marginTop: '3vh',
+  }, 
+  gohereButton: {
+    marginTop: '3vh',
+  },
+});
+
+
+export default CardForm;
