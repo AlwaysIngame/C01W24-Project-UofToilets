11246eecfe344e24dc974a4c841a68013c926eff
Sebastian <sebi.jingoi@gmail.com>
2024-03-05 10:41:02 -0500

    created mongo connection

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,25 @@
-console.log("Hello world")
+import express from 'express';
+import http from 'http';
+import bodyParser from 'body-parser'
+import { MongoClient, ObjectId } from 'mongodb';
+
+const app = express();
+const PORT = 4000;
+const mongoURL = "mongodb://127.0.0.1:27017";
+const dbName = "gohere"
+
+let db;
+
+async function connectToMongo() {
+    const client = new MongoClient(mongoURL);
+
+    try {
+        await client.connect();
+        console.log("Connected to database " + dbName);
+        db = client.db(dbName);
+    } catch (error) {
+        console.error("Error connecting to MongoDB: " + error);
+    }
+}
+
+connectToMongo();
