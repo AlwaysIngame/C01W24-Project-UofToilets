06c9f7fdb39f1e3c5fd92b659b7f2daedda64ebf
Isaiah Velasquez <iavelasquez2004@gmail.com>
2024-03-11 18:05:04 -0400

    Added bottom-sheet for search

--- app/components/navbar.js
+++ app/components/navbar.js
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import TestComponent from './testcomponent';
@@ -12,10 +13,12 @@ export default function TabNavigation() {
   const Tab = createBottomTabNavigator();
 
   return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
     <Tab.Navigator>
       <Tab.Screen name="Home" component={MapScreen} />
       <Tab.Screen name="Card" component={UserAccessCard}/>
       <Tab.Screen name="Washrooms" component={ScrollableList}/>
     </Tab.Navigator>
+    </GestureHandlerRootView>
   );
 }
