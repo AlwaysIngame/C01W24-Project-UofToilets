1e0ecbeef4c61100e07d2984c5094ba991fce00a
unknown <kieran.r.hansen@gmail.com>
2024-03-18 21:37:27 -0400

    Created page for settings

---
+++ app/components/SettingsScreen/SettingsScreen.js
@@ -0,0 +1,15 @@
+import { StyleSheet, Dimensions, View, Text, Linking } from 'react-native';
+
+const SettingsScreen = () => {
+
+  //Functions
+
+  //Render component
+  return (
+    <View>
+      
+    </View>
+  );
+};
+
+export default SettingsScreen;
