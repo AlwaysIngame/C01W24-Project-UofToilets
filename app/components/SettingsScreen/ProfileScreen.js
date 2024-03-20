b6911f35cfd1860115d5cfd6c435ef5f11ab6dcc
unknown <kieran.r.hansen@gmail.com>
2024-03-19 23:21:14 -0400

    Added bottom sheet and background component

--- app/components/SettingsScreen/ProfileScreen.js
+++ app/components/SettingsScreen/ProfileScreen.js
@@ -1,7 +1,8 @@
-import { useState } from 'react';
-import { StyleSheet, Dimensions, View, Text, TouchableOpacity } from 'react-native';
+import React, { useState, useCallback, useMemo, useRef } from 'react';
+import { StyleSheet, Dimensions, View, Text, TouchableOpacity, Modal } from 'react-native';
 import { TextInput } from 'react-native-gesture-handler';
 import { AntDesign } from '@expo/vector-icons';
+import BottomSheet, { BottomSheetBackdrop, BottomSheetView } from '@gorhom/bottom-sheet';
 
 const vh = Dimensions.get('window').height / 100;
 const vw = Dimensions.get('window').width / 100;
@@ -31,49 +32,83 @@ const ProfileScreen = () => {
     //return back to the previous navigation and save changes
   }
 
+  function openSheet(){
+    bottomSheetRef.current.expand()
+  }
+
+  function closeSheet(){
+    bottomSheetRef.current.collapse()
+  }
+
+  const bottomSheetRef = useRef();
+
+  const renderBackdrop = useCallback((props) => 
+    <BottomSheetBackdrop {...props} style={{backgroundColor: 'grey', position: 'absolute', width: 100*vw, height: 100*vh}} appearsOnIndex={0} disappearsOnIndex={-1}/>
+  );
+
   //Render
   return(
     <View style={{flex: 1, padding: 24, backgroundColor: 'white',}}>
       <View style={{borderBottomColor: '#cccccc', 
-                    borderBottomWidth: 1 }}>
-        <Text style={{fontSize: 13}}>First Name</Text>
+                    borderBottomWidth: 1,
+                    marginBottom: 2*vh}}>
+        <Text style={{fontSize: 13, marginBottom: 1*vh}}>First Name</Text>
         <TextInput style={{fontSize: 16}}
                    value={firstName}
+                   selectionColor='black'
                    placeholder='Enter first name'
                    onChangeText={setFirstName}/>
       </View>
 
       <View style={{borderBottomColor: '#cccccc', 
-                    borderBottomWidth: 1, }}>
-        <Text style={{fontSize: 13}}>Last Name</Text>
+                    borderBottomWidth: 1,
+                    marginBottom: 2*vh }}>
+        <Text style={{fontSize: 13, marginBottom: 1*vh}}>Last Name</Text>
         <TextInput style={{fontSize: 16}}
                    value={lastName}
+                   selectionColor='black'
                    placeholder='Enter last name'
                    onChangeText={setLastName}/>
       </View>
 
       <View style={{borderBottomColor: '#cccccc', 
-                    borderBottomWidth: 1, }}>
-      <Text style={{fontSize: 13}}>Health Condition</Text>
+                    borderBottomWidth: 1,
+                    marginBottom: 2*vh}}>
+      <Text style={{fontSize: 13, marginBottom: 1*vh}}>Health Condition</Text>
       <TouchableOpacity style={{flexDirection: 'row',
-                                marginVertical: 8}}>
+                                marginVertical: 8}}
+                        onPress={openSheet}>
           <Text style={{fontSize: 16}}>{condition}</Text>
           <AntDesign name="down" size={20} color="#cccccc" style={{position: 'absolute', right: 0, bottom: 0 }}/>
         </TouchableOpacity>
       </View>
 
-
+      <BottomSheet
+        ref={bottomSheetRef}
+        index={-1}
+        snapPoints={['50%']}
+        style={styles.sheetShadow}
+        backdropComponent={renderBackdrop}>
+        <BottomSheetView>
+          <Text>Awesome 🎉</Text>
+        </BottomSheetView>
+      </BottomSheet>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 24,
-    paddingRight: 12,
-    paddingTop: 34,
-    backgroundColor: 'white',
+  sheetShadow: {
+    backgroundColor: 'rgba(255, 255, 255,0)',  // <==== HERE
+    borderRadius: 24,
+    shadowColor: '#000000',
+    shadowOffset: {
+      width: 0,
+      height: 8,
+    },
+    shadowOpacity: 0.1,
+    shadowRadius: 24,
+    elevation: 10,
   },
 });
 
