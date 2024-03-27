1557b91c0b556a75fdf1960a5a2e717f285d10ab
Sebastian <sebi.jingoi@gmail.com>
2024-03-26 21:26:02 -0400

    Added get all user washrooms

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -121,7 +121,7 @@ app.delete("/deleteUser", express.json(), async (req, res) => {
         }
 
         jwt.verify(token, "secret-key", async (err, decoded) => {
-            if (err || jwtDecode<UsernameToken>(token).username != username) { return res.status(401).send("Unauthorized."); }
+            if (err || jwtDecode<UsernameToken>(token).username != username) { return res.status(401).send({ error: "Unauthorized" }); }
             const result = await userCollection.deleteOne({ username: username });
             // Delete all other information
             
@@ -135,6 +135,86 @@ app.delete("/deleteUser", express.json(), async (req, res) => {
         res.status(500).json({ error: error.message });
     }
 });
+
+
+app.post("/addWashroom", express.json(), async (req, res) => {
+    try {
+        
+        let new_washroom: Washroom = req.body;
+        new_washroom.approved = false;
+        new_washroom.id = randomUUID();
+        if (!isValidDatabaseWashroom(req.body)) {
+            res.status(404).json({ error: "Invalid request body." });
+            return;
+        }
+        if (req.headers.authorization) {
+            const token = req.headers.authorization.split(" ")[1];
+            new_washroom.owner_username = jwtDecode<UsernameToken>(token).username;
+        }
+        const washroomCollection = db.collection(COLLECTIONS.Washrooms);
+        const result = washroomCollection.insertOne(new_washroom);
+        res.status(201).json({
+            response: "Washroom added successfully.",
+            washroomID: new_washroom.id,
+        })
+    } catch (error: any) {
+        res.status(500).json({ error: error.message });
+    }
+})
+
+app.delete("/deleteWashroom/:id", express.json(), async (req, res) => {
+    try {
+        
+        let id = req.params.id;
+        
+        if (!req.headers.authorization) {
+            res.status(401).json("Not Authorized.");
+            return;
+        }
+        const token = req.headers.authorization.split(" ")[1];
+        
+        const washroomCollection = db.collection(COLLECTIONS.Washrooms);
+        
+        jwt.verify(token, "secret-key", async (err, decoded) => {
+            if (err) { return res.status(401).send({ error: "Unauthorized" }); }
+            const jwtDecoded = jwtDecode<UsernameToken>(token);
+            const result = await washroomCollection.deleteOne({owner_username: jwtDecoded.username, id: id});
+            
+            if (result.deletedCount === 1) {
+                res.status(200).json({ response: "Washroom with id " + id + " deleted."});
+            } else {
+                res.status(404).json({ error: "Could not find washroom associated with token to delete." })
+            }
+        });
+    } catch (error: any) {
+        res.status(500).json({ error: error.message });
+    }
+})
+
+app.get("/getUserWashrooms", express.json(), async (req, res) => {
+    const default_radius = 3000;
+    try {
+
+        if (!req.headers.authorization) {
+            res.status(401).json({ error: "Unauthorized" });
+            return;
+        }
+        const token = req.headers.authorization.split(" ")[1];
+
+        const washroomCollection = db.collection(COLLECTIONS.Washrooms);
+        
+        jwt.verify(token, "secret-key", async (err, decoded) => {
+            if (err) { return res.status(401).send({ error: "Unauthorized" }); }
+            const jwtDecoded = jwtDecode<UsernameToken>(token);
+            const washrooms = await washroomCollection.find({owner_username: jwtDecoded.username}).toArray();
+            res.status(200).json({ response: washrooms });
+        });
+
+    } catch (error: any) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
 
 app.get("/getWashroomByLocation/:payload", express.json(), async (req, res) => {
     const default_radius = 3000;
@@ -152,8 +232,8 @@ app.get("/getWashroomByLocation/:payload", express.json(), async (req, res) => {
         if (!request.radius) {
             request.radius = default_radius;
         }
-        const userCollection = db.collection(COLLECTIONS.Washrooms);
-        const washrooms = (await userCollection.find().toArray()).filter((washroom) => {
+        const washroomCollection = db.collection(COLLECTIONS.Washrooms);
+        const washrooms = (await washroomCollection.find().toArray()).filter((washroom) => {
             // if (!washroom.approved) return false;
             let dist = haversineDistance({ lat: washroom.latitude, lon: washroom.longitude },
                               { lat: request.latitude, lon: request.longitude });
@@ -166,60 +246,3 @@ app.get("/getWashroomByLocation/:payload", express.json(), async (req, res) => {
         res.status(500).json({ error: error.message });
     }
 });
-
-app.post("/addWashroom", express.json(), async (req, res) => {
-    try {
-
-        let new_washroom: Washroom = req.body;
-        new_washroom.approved = false;
-        new_washroom.id = randomUUID();
-        if (!isValidDatabaseWashroom(req.body)) {
-            res.status(404).json({ error: "Invalid request body." });
-            return;
-        }
-        if (req.headers.authorization) {
-            const token = req.headers.authorization.split(" ")[1];
-            console.log(token);
-            new_washroom.owner_username = jwtDecode<UsernameToken>(token).username;
-        }
-        const washroomCollection = db.collection(COLLECTIONS.Washrooms);
-        const result = washroomCollection.insertOne(new_washroom);
-        res.status(201).json({
-            response: "Washroom added successfully.",
-            washroomID: new_washroom.id,
-        })
-    } catch (error: any) {
-        res.status(500).json({ error: error.message });
-    }
-})
-
-app.delete("/deleteWashroom/:id", express.json(), async (req, res) => {
-    try {
-
-        let id = req.params.id;
-
-        if (!req.headers.authorization) {
-            res.status(401).json("Not Authorized.");
-            return;
-        }
-        const token = req.headers.authorization.split(" ")[1];
-
-        const washroomCollection = db.collection(COLLECTIONS.Washrooms);
-
-        jwt.verify(token, "secret-key", async (err, decoded) => {
-            if (err) { return res.status(401).send("Unauthorized."); }
-            const jwtDecoded = jwtDecode<UsernameToken>(token);
-            const result = await washroomCollection.deleteOne({owner_username: jwtDecoded.username, id: id});
-            
-            if (result.deletedCount === 1) {
-                res.status(200).json({ response: "Washroom with id " + id + " deleted."});
-            } else {
-                res.status(404).json({ error: "Could not find washroom associated with token to delete." })
-            }
-        });
-    } catch (error: any) {
-        res.status(500).json({ error: error.message });
-    }
-})
-
-
