55d27ae13956ad345e417d6e2adab4d236810c62
Sebastian <sebi.jingoi@gmail.com>
2024-03-28 19:55:24 -0400

    API endpoints

--- adminportal/src/Pages/Login.js
+++ adminportal/src/Pages/Login.js
@@ -9,6 +9,7 @@ function LoginPage(){
   const [loginError, setLoginError] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
   let navigate = useNavigate();
+  const [password, setPassword] = useState("");
 
   async function handleSubmit(e){
     e.preventDefault();
@@ -22,20 +23,22 @@ function LoginPage(){
     // setErrorMessage("*Username and Password cannot be blank")
     //} //*Incorrect Username or Password
 
-    /* -- Call and validate login -- Uncomment when confirmed working
+    //-- Call and validate login -- Uncomment when confirmed working
     try {
-      const loginRes = await fetch(`${SERVER_URL}/loginAdmin`, {
+      const loginRes = await fetch(`${SERVER_URL}/loginUser`, {
+        mode: "cors",
         method: "POST",
         headers: {
-            "Content-Type": "application/json",
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({
-            username: formJson.username,
-            password: formJson.password,
+          username: "admin",
+          password: "chickennuggets",
         })
       })
     
       const loginBody = await loginRes.json();
+      console.log(loginBody);
 
       if (loginRes.ok) {
         setErrorMessage("");
@@ -50,10 +53,7 @@ function LoginPage(){
     } catch (error) {
       setErrorMessage("Login failure: " + error);
     }
-    */
-    
-    //For now, we navigate, remove once validation is working
-    navigate('/admin/dashboard')
+
 
   };
 
@@ -63,8 +63,8 @@ function LoginPage(){
       <div className='loginContainer'>
         <h1 className='pageTitle'>GoHere Portal</h1>
         <form method='post' onSubmit={handleSubmit} className='loginForm'>
-          <input className='textInput' name="username" placeholder="Username"/>
-          <input className='textInput' name="password" type="password" placeholder='Password'/>
+          <input className='textInput' name="username" placeholder="admin" disabled={true} defaultValue={"admin"}></input>
+          <input className='textInput' name="password" type="password" placeholder='Password' onChange={text => setPassword(text)}/>
           {loginError && (<p className='errorMsg'>{errorMessage}</p>)}          
           <button className='loginButton' type='submit'>Login</button>
         </form> 
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,8 +2,11 @@ import express from 'express';
 import http from 'http';
 import bodyParser from 'body-parser'
 import { MongoClient, ObjectId, Db } from 'mongodb';
+import cors from 'cors';
+
 
 const app = express();
+app.use(cors());
 const PORT = 4000;
 let mongoURL: string;
 if (process.env.ENV === 'Docker') {
@@ -14,7 +17,7 @@ if (process.env.ENV === 'Docker') {
 const dbName = "gohere"
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
-import { COLLECTIONS, Washroom, WashroomLocationReqPayload, isValidDatabaseWashroom } from "./databasetypes";
+import { COLLECTIONS, NewsPost, Washroom, WashroomLocationReqPayload, isValidDatabaseWashroom, isValidNewsPost } from "./databasetypes";
 import haversineDistance from 'haversine-distance';
 import { randomUUID } from 'crypto';
 import { jwtDecode } from 'jwt-decode';
@@ -136,101 +139,103 @@ app.delete("/deleteUser", express.json(), async (req, res) => {
     }
 });
 
-app.post("/registerAdmin", express.json(), async (req, res) => {
-    try {
-        const { username, password } = req.body;
-        if (!username || !password) {
-            return res.status(400).json({error: "Missing username or password."});
-        }
+// app.post("/registerAdmin", express.json(), async (req, res) => {
+//     try {
+//         const { username, password } = req.body;
+//         if (!username || !password) {
+//             return res.status(400).json({error: "Missing username or password."});
+//         }
 
-        console.log("Register");
+//         console.log("Register");
 
-        const adminCollection = db.collection(COLLECTIONS.Admins);
-        const existingAdmin = await adminCollection.findOne({ username });
-        if (existingAdmin) {
-            return res.status(400).json({ error: "Username already exists."})
-        }
+//         const adminCollection = db.collection(COLLECTIONS.Admins);
+//         const existingAdmin = await adminCollection.findOne({ username });
+//         if (existingAdmin) {
+//             return res.status(400).json({ error: "Username already exists."})
+//         }
 
-        const hashedPassword = await bcrypt.hash(password, 10);
-        await adminCollection.insertOne({
-            username,
-            password: hashedPassword,
-        });
+//         const hashedPassword = await bcrypt.hash(password, 10);
+//         await adminCollection.insertOne({
+//             username,
+//             password: hashedPassword,
+//         });
 
-        const token = jwt.sign({ username }, "secret-key", { expiresIn: "1h" });
-        res.status(201).json({ response: "User registered successfully.", token: token });
-    } catch (error: any) {
-        res.status(500).json({ error: error.message });
-    }
-});
+//         const token = jwt.sign({ username }, "secret-key", { expiresIn: "1h" });
+//         res.status(201).json({ response: "User registered successfully.", token: token });
+//     } catch (error: any) {
+//         res.status(500).json({ error: error.message });
+//     }
+// });
 
-app.post("/loginAdmin", express.json(), async (req, res) => {
-    try {
-        const { username, password } = req.body;
+// app.post("/loginAdmin", express.json(), async (req, res) => {
+//     try {
+//         const { username, password } = req.body;
     
-        // Basic body request check
-        if (!username || !password) {
-        return res
-            .status(400)
-            .json({ error: "*Missing username or password" });
-        }
+//         // Basic body request check
+//         if (!username || !password) {
+//         return res
+//             .status(400)
+//             .json({ error: "*Missing username or password" });
+//         }
     
-        // Find username in database
-        const adminCollection = db.collection(COLLECTIONS.Admins);
-        const admin = await adminCollection.findOne({ username });
+//         // Find username in database
+//         const adminCollection = db.collection(COLLECTIONS.Admins);
+//         const admin = await adminCollection.findOne({ username });
     
-        // Validate user against hashed password in database
-        if (admin && (await bcrypt.compare(password, admin.password))) {
-            const token = jwt.sign({ username }, "secret-key", { expiresIn: "1h" });
+//         // Validate user against hashed password in database
+//         if (admin && (await bcrypt.compare(password, admin.password))) {
+//             const token = jwt.sign({ username }, "secret-key", { expiresIn: "1h" });
         
-            // Send JSON Web Token to valid user
-            res.json({ response: "Admin logged in succesfully.", token: token }); //Implicitly status 200
-        } else {
-        res.status(401).json({ error: "*Incorrect Username or Password" });
-        }
-    } catch (error: any) {
-        res.status(500).json({ error: error.message });
-    }
-});
+//             // Send JSON Web Token to valid user
+//             res.json({ response: "Admin logged in succesfully.", token: token }); //Implicitly status 200
+//         } else {
+//         res.status(401).json({ error: "*Incorrect Username or Password" });
+//         }
+//     } catch (error: any) {
+//         res.status(500).json({ error: error.message });
+//     }
+// });
 
-app.delete("/deleteAdmin", express.json(), async (req, res) => {
-    try {
-        const username: string | undefined = req.body.username;
-        const password: string | undefined = req.body.password;
+// app.delete("/deleteAdmin", express.json(), async (req, res) => {
+//     try {
+//         const username: string | undefined = req.body.username;
+//         const password: string | undefined = req.body.password;
 
-        if (!req.headers.authorization) {
-            return res.status(401).json({ error: "Missing authentication token"});
-        }
+//         if (!req.headers.authorization) {
+//             return res.status(401).json({ error: "Missing authentication token"});
+//         }
 
-        const token = req.headers.authorization.split(" ")[1];
+//         const token = req.headers.authorization.split(" ")[1];
 
-        if (!username || !password) {
-        return res.status(400).json({ error: "Missing username or password." });
-        }
+//         if (!username || !password) {
+//         return res.status(400).json({ error: "Missing username or password." });
+//         }
 
-        const adminCollection = db.collection(COLLECTIONS.Admins);
-        const admin = await adminCollection.findOne({ username });
+//         const adminCollection = db.collection(COLLECTIONS.Admins);
+//         const admin = await adminCollection.findOne({ username });
     
-        if (!(admin && (await bcrypt.compare(password, admin.password)))) {
-            return res.status(401).json({ error: "Incorrect username or password." });
-        }
+//         if (!(admin && (await bcrypt.compare(password, admin.password)))) {
+//             return res.status(401).json({ error: "Incorrect username or password." });
+//         }
 
-        jwt.verify(token, "secret-key", async (err, decoded) => {
-            if (err) { return res.status(401).send("Unauthorized."); }
-            console.log(decoded);
-            const result = await adminCollection.deleteOne({ username: username });
-            // Delete all other information
+//         jwt.verify(token, "secret-key", async (err, decoded) => {
+//             if (err) { return res.status(401).send("Unauthorized."); }
+//             console.log(decoded);
+//             const result = await adminCollection.deleteOne({ username: username });
+//             // Delete all other information
             
-            if (result.deletedCount === 1) {
-                res.status(200).json({ response: "Admin " + username + " deleted."});
-            } else {
-                res.status(404).json({ error: "Could not find username to delete." })
-            }
-        });
-    } catch (error: any) {
-        res.status(500).json({ error: error.message });
-    }
-});app.post("/addWashroom", express.json(), async (req, res) => {
+//             if (result.deletedCount === 1) {
+//                 res.status(200).json({ response: "Admin " + username + " deleted."});
+//             } else {
+//                 res.status(404).json({ error: "Could not find username to delete." })
+//             }
+//         });
+//     } catch (error: any) {
+//         res.status(500).json({ error: error.message });
+//     }
+// });
+
+app.post("/addWashroom", express.json(), async (req, res) => {
     try {
         
         let new_washroom: Washroom = req.body;
@@ -308,6 +313,31 @@ app.get("/getUserWashrooms", express.json(), async (req, res) => {
     }
 });
 
+app.get("/getUnverifiedWashrooms", express.json(), async (req, res) => {
+    const default_radius = 3000;
+    try {
+
+        if (!req.headers.authorization) {
+            res.status(401).json({ error: "Unauthorized" });
+            return;
+        }
+        const token = req.headers.authorization.split(" ")[1];
+
+        const washroomCollection = db.collection(COLLECTIONS.Washrooms);
+        
+        jwt.verify(token, "secret-key", async (err, decoded) => {
+            if (err) { return res.status(401).send({ error: "Unauthorized" }); }
+            const jwtDecoded = jwtDecode<UsernameToken>(token);
+            if (jwtDecoded.username != "admin") {return res.status(401).send({ error: "Unauthorized" });}
+            const washrooms = await washroomCollection.find({approved: false}).toArray();
+            res.status(200).json({ response: washrooms });
+        });
+
+    } catch (error: any) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
 
 app.get("/getWashroomByLocation/:payload", express.json(), async (req, res) => {
     const default_radius = 3000;
@@ -335,6 +365,112 @@ app.get("/getWashroomByLocation/:payload", express.json(), async (req, res) => {
 
         res.status(200).json({ response: washrooms })
         
+    } catch (error: any) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+
+app.post("/postNews", express.json(), async (req, res) => {
+    try {
+        
+        let newNewsPost: NewsPost = req.body;
+        newNewsPost.id = randomUUID();
+        if (!isValidNewsPost(newNewsPost)) {
+            res.status(404).json({ error: "Invalid request body." });
+            return;
+        }
+        if (!req.headers.authorization) {
+            res.status(401).json("Not Authorized.");
+            return;
+        }
+        const token = req.headers.authorization.split(" ")[1];
+        jwt.verify(token, "secret-key", async (err, decoded) => {
+            if (err) { return res.status(401).send({ error: "Unauthorized" }); }
+            const jwtDecoded = jwtDecode<UsernameToken>(token);
+            if (jwtDecoded.username != "admin") {return res.status(401).send({ error: "Unauthorized" });}
+            const newsCollection = db.collection(COLLECTIONS.News);
+            const result = await newsCollection.insertOne(newNewsPost);
+            if (result.insertedId) {
+                res.status(201).json({
+                    response: "News post added successfully.",
+                    newsID: newNewsPost.id,
+                })
+            } else {
+                res.status(500).json({ error: "Could not add news post." });
+            }
+        });
+    } catch (error: any) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+app.patch("/editNews/:id", express.json(), async (req, res) => {
+    try {
+        let id = req.params.id;
+        let newNewsPost: NewsPost = req.body;
+        newNewsPost.id = id;
+        if (!isValidNewsPost(newNewsPost)) {
+            res.status(404).json({ error: "Invalid request body." });
+            return;
+        }
+        if (!req.headers.authorization) {
+            res.status(401).json("Not Authorized.");
+            return;
+        }
+        const token = req.headers.authorization.split(" ")[1];
+        jwt.verify(token, "secret-key", async (err, decoded) => {
+            if (err) { return res.status(401).send({ error: "Unauthorized" }); }
+            const jwtDecoded = jwtDecode<UsernameToken>(token);
+            if (jwtDecoded.username != "admin") {return res.status(401).send({ error: "Unauthorized" });}
+            const newsCollection = db.collection(COLLECTIONS.News);
+            const result = await newsCollection.updateOne({id: id}, {$set: newNewsPost});
+            if (result.modifiedCount === 1) {
+                res.status(200).json({ response: "News post updated successfully."});
+            } else {
+                res.status(404).json({ error: "Could not find news post to update." })
+            }
+        });
+    } catch (error: any) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+app.get("/getNews", express.json(), async (req, res) => {
+    try {
+        const newsCollection = db.collection(COLLECTIONS.News);
+        const news = await newsCollection.find().toArray();
+        res.status(200).json({ response: news });
+    } catch (error: any) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+app.delete("/deleteNews/:id", express.json(), async (req, res) => {
+    try {
+        
+        let id = req.params.id;
+        
+        if (!req.headers.authorization) {
+            res.status(401).json("Not Authorized.");
+            return;
+        }
+        const token = req.headers.authorization.split(" ")[1];
+        
+        const newsCollection = db.collection(COLLECTIONS.News);
+        
+        jwt.verify(token, "secret-key", async (err, decoded) => {
+            if (err) { return res.status(401).send({ error: "Unauthorized" }); }
+            const jwtDecoded = jwtDecode<UsernameToken>(token);
+            if (jwtDecoded.username != "admin") {return res.status(401).send({ error: "Unauthorized" });}
+            const result = await newsCollection.deleteOne({id: id});
+            
+            if (result.deletedCount === 1) {
+                res.status(200).json({ response: "News post deleted successfully."});
+            } else {
+                res.status(404).json({ error: "Could not find news post to delete." })
+            }
+        });
     } catch (error: any) {
         res.status(500).json({ error: error.message });
     }
